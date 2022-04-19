acb8f8de8c68c891607059de76b1cc81767d55b0
Alan Perez <alanperez@alans-mbp.mynetworksettings.com>
2022-04-19 19:30:59 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+const mymap = L.map('issMap').setView([0, 0], 1);
+    const attribution = 
+        '&copy; <a href="https://www.openstreetmap.org/copyright"> OpenStreetMap </a> contributors';
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, {attribution});
+    tiles.addTo(mymap);
+
+    // making a marker with a custom icon 
+    const issIcon= L.icon({
+        iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/ISS_spacecraft_model_1.png/320px-ISS_spacecraft_model_1.png',
+        iconSize:[50,32],
+        iconAnchor: [25,16]
+    });
+    let marker = L.marker([0,0], {icon: issIcon}).addTo(mymap);
+    mymap.on('zoomed', function() {
+        const zoom = mymap.getZoom() + 1;
+        const w = 50 * zoom;
+        const h = 32 * zoom;
+        issIcon.options.iconSize = [w,h];
+        issIcon.options.izonAnchor = [w/2,h/2];
+        mymap.removeLayer(marker);
+        let latlng = marker.getLatLng();
+        marker = L.marker([0,0], {icon: issIcon}).addTo(mymap);
+        marker.setLatLng(latlng);
+    })
+
+
+    //const marker = L.marker([0,0], { icon: issIcon}).addTo(mymap);
+    const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+    let firstTime = true;
+
+
+    async function getISS() {
+        const response = await fetch (api_url);
+        const data = await response.json();
+        const {latitude, longitude} = data;
+
+       // L.marker([latitude, longitude]).addTo(mymap);
+        marker.setLatLng([latitude, longitude]);
+        if (firstTime) {
+            mymap.setView([latitude, longitude], 2);
+            firstTime= false;
+        }
+       // mymap.setView([latitude, longitude], 2);
+        document.getElementById('lat').textContent = latitude.toFixed(2);
+        document.getElementById('lon').textContent = longitude.toFixed(2);
+      
+    }
+    getISS();
+
+    setInterval(getISS,1000);
